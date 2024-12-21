bec02c4916b758a3cb17845707b00fd87a75f67e
thisisfaisalhere <thisisfaisalhere@gmail.com>
2024-12-21 13:18:56 +0530

    chore: package clean up

--- dev-dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/track-hero/dev-sw.js?dev-sw', { scope: '/track-hero/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   plugins: [
     vue(),
     VitePWA({
@@ -17,5 +17,5 @@ export default defineConfig({
   server: {
     port: 8000,
   },
-  base: '/track-hero/',
-})
+  base: command === 'serve' ? '' : '/track-hero/',
+}))
